b5e16a63c7876f25578d17d8bab778f3b26b3bf6
Kartik <kartik2005221@proton.me>
2024-06-01 19:56:34 +0530

    v2.3

---
+++ scripts/iam.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var typed = new Typed('#typed-output', {
+	  strings: ["a Cybersecurity Student", "a Web Developer", "a Firefox Extension Developer", "a Lifelong Learner"],
+	  typeSpeed: 60,
+	  backSpeed: 40,
+	  backDelay: 1500,
+	  loop: true,
+	  showCursor: true,
+	  cursorChar: '|',
+	});
+  });
